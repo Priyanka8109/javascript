5098d6b3ba557d77383635eb12555e029bc1d176
Priyanka8109 <priyankarajpal17092004@gmail.com>
2025-08-23 21:54:43 +0530

    Linking html,css with JS

---
+++ lec 1/index.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+console.warn("Hello world")
+console.error("not found!!!")
+
+function callFunc()
+{
+    if (true)
+    {
+        let foo = 5
+    }
+    console.log(foo)
+    console.log("Btn is clicked")
+    console.log('test')
+}
