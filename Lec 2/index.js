e472152619dbc2c68562178cd30ce2e80e5d6e71
Priyanka8109 <priyankarajpal17092004@gmail.com>
2025-08-24 14:03:58 +0530

    DATA TYPES

---
+++ Lec 2/index.js
@@ -0,0 +1,53 @@
+function clickButton(){
+    prompt('you are here')
+    alert('you are here')
+   let value = prompt('YOU ARE HERE')
+   console.log(value)
+}
+
+let x = 'Test'
+console.log(x.toLowerCase());
+
+//Multiple parameters
+console.log(1,2,3,4,5)
+console.log(1+"-"+2+"-"+3)
+console.log(1,"-",2,"-",3)
+
+
+/*print krega kyunki == hai sirf value check krega print bhi krega without checking datatype
+bhale number aur string saath me hai*/
+let stringNumber = "5"
+if(stringNumber == 5){
+    console.log("inside if!!")
+
+
+    //print nhi krega kyunki === hai value aur data type dono check krega toh datatype ek number 
+    //aur print string kr rhe hai isliye error dega
+let xyz = "5";
+if(xyz === 5){
+    console.log("inside if")
+}
+
+console.log("5"-"3")
+console.log("4"+"3")
+console.log("4"*"3")
+console.log("4"/"2")
+console.log("abc"-"3")
+console.log(typeof NaN)
+console.log('hello')
+//number,string,boolean,null,undefined,NaN
+
+
+//Array,Objects
+
+//array - internally stored in an object
+let arr = [1,2,3,4,5]
+console.log(arr)
+
+//for loop -for array
+//sb alg alg print ho jayenge
+for(let i = 0;i<5;i++)
+    console.log(arr[i]);
+
+//Object is a key value
+}
---
+++ Lec 2/second.js
@@ -0,0 +1,3 @@
+// to run individually we dont need html css we have to use nodejs
+//right click on lec to run in integrated terminal and write node secondjs
+console.log(`hello world`)
